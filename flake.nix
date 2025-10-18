c43f930c69afd75376f2a769c1f202d727b39d2b
jakublaba <jakub.maciej.laba@gmail.com>
2025-10-19 01:23:14 +0200

    Stop tracking hardware-configuration.nix and use the autogenerated one instead

--- flake.nix
+++ flake.nix
@@ -3,14 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    etcNixos = {
+      url = "path:/etc/nixos";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, etcNixos, ... }@inputs: {
     # nixosConfiguration.<hostname> - remember to adjust
     nixosConfigurations.krb = nixpkgs.lib.nixosSystem {
       modules = [
-        # ./configuration.nix
-        ./hardware-configuration.nix
+        "${etcNixos}/hardware-configuration.nix"
         ./modules/amdgpu.nix
         ./modules/audio.nix
         ./modules/boot.nix
