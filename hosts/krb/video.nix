42d55394d8b454f190cc511e8060c680c0ae3584
jakublaba <jakub.maciej.laba@gmail.com>
2025-10-20 01:41:02 +0200

    Replace lact with corectrl

--- hosts/krb/video.nix
+++ hosts/krb/video.nix
@@ -5,7 +5,5 @@
   hardware.amdgpu.overdrive.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-  environment.systemPackages = with pkgs; [ lact ];
-  systemd.packages = with pkgs; [ lact ];
-  systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+  environment.systemPackages = with pkgs; [ corectrl ];
 }
