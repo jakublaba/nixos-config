bb91d9af202e7126430b4144c5cebe1ac1cf8e50
jakublaba <jakub.maciej.laba@gmail.com>
2025-10-20 20:05:10 +0200

    add sudo pwfeedback

--- modules/users.nix
+++ modules/users.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  security.sudo.extraConfig = ''
+    Defaults pwfeedback
+  '';
+
   # Don't forget to set a password with 'passwd'
   users.users.kuba = {
     isNormalUser = true;
