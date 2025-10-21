c8078def857e2403a561560dafe865819e338c5b
jakublaba <jakub.maciej.laba@gmail.com>
2025-10-21 15:17:05 +0200

    nixfmt -> alejandra

--- modules/packages.nix
+++ modules/packages.nix
@@ -6,6 +6,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    alejandra
     bat
     btop
     curl
@@ -19,7 +20,6 @@
     iotop
     mangohud
     nethogs
-    nixfmt-rfc-style
     pciutils
     ripgrep
     tree
