ef50b708c5f00d2d9a4b13939e01c641185cbfed
jakublaba <jakub.maciej.laba@gmail.com>
2025-10-22 21:30:16 +0200

    docker

--- hosts/pc/virtualization/default.nix
+++ hosts/pc/virtualization/default.nix
@@ -1,6 +1,11 @@
 {...}: {
+  # normal vms
   programs.virt-manager.enable = true;
   users.groups.libvirtd.members = ["kuba"];
   virtualisation.libvirtd.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
+
+  # docker
+  virtualisation.docker.enable = true;
+  users.groups.docker.members = ["kuba"];
 }
