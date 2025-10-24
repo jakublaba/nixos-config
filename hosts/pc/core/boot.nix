94ccb57dd11fd4033707af733a054487a5180349
jakublaba <jakub.maciej.laba@gmail.com>
2025-10-24 14:09:08 +0200

    change boot timeout

--- hosts/pc/core/boot.nix
+++ hosts/pc/core/boot.nix
@@ -6,6 +6,9 @@
         configurationLimit = 3;
       };
       efi.canTouchEfiVariables = true;
+
+      # spam space for systemd-boot menu
+      timeout = 0;
     };
 
     kernelPackages = pkgs.linuxPackages_latest;
