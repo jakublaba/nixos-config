a2c600ecba73c29c77a107ec46e05759295ec79d
jakublaba <jakub.maciej.laba@gmail.com>
2025-12-08 23:21:09 +0100

    bump flake input version 25.05 -> 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS config - jakublaba";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = {
